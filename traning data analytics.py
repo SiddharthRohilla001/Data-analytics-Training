5ab6e7e46c7709a38a98f71e9e8b05d543c3f664
SiddharthRohilla001 <siddharthrohilla41@gmail.com>
2025-09-22 12:08:25 +0530

    Add files via upload

---
+++ traning data analytics.py
@@ -0,0 +1,24 @@
+'''print(12345678)
+print("siddharth")'''
+
+'''a = int(input ("Enter the first number:"))
+b = int(input ("Enter the first number:"))
+sum = a + b
+print (sum)
+sub = a - b
+print(sub)
+div = a/b
+print(div)
+mul = a*b
+print(mul)'''
+
+'''a = input("Enter your name : ")
+b = int(input("Enter your age : "))fj
+c = input("Enter your city name : ")
+print("naam = ",a,"\n","umar = ", b,"\n", "pta = " ,c)'''
+
+a = int (input("Enter a number: "))
+if ((a > 0) and (a < 18)):
+    print("You are not eligible to vote")
+elif ((a >= 18) and (a < 65)):
+    print("You are eligible to vote")
